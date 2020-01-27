9691722525364ca69d81cd29a058faf73eebb81c
svejlgaard <60341038+svejlgaard@users.noreply.github.com>
2020-01-27 14:14:02 +0100

    Change to setuptools based

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-from setuptools import setup
+import setuptools
 
-setup(
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
   name = 'pyzar',
   url = 'https://github.com/svejlgaard/pyzar',
   author = 'Simone Vejlgaard Nielsen',
@@ -12,9 +16,12 @@ setup(
   
   
   description = 'A package to detect spectral lines, determine redshift and measure equivalent width of QSO and QAL.'
-  long_description = open('README.md').read(),
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
 
   
   scripts = ['scripts/eqw.py'],
+  packages = setuptools.find_packages(),
   
+  python_requires = '>=3.6',
 )
