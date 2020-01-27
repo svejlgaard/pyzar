690bbcf5846d2e07c1013d0fbc1758335e8ffbda
svejlgaard <60341038+svejlgaard@users.noreply.github.com>
2020-01-27 13:31:05 +0100

    Creating a setup.py
    First version of setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+  name = 'pyzar',
+  url = 'https://github.com/svejlgaard/pyzar',
+  author = 'Simone Vejlgaard Nielsen',
+  author_email = 'fgj787@alumni.ku.dk',
+  
+  
+  install_requires = ['numpy','pandas', 'pyspeckit','astropy','matplotlib','sklearn','scipy','extinction','datatime','json']
+  version = '20.01'
+  
+  
+  description = 'A package to detect spectral lines, determine redshift and measure equivalent width'
+  long_description = open('README.rst').read(),
+)
